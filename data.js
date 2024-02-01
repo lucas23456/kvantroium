3aee65c6f480d34c0ae226dc4ea09e68875ae733
John Doe <johndoe@email.com>
2024-02-01 19:13:38 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,214 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.9552389750666404,
+        "pitch": 0.05659588491006495,
+        "fov": 1.4308103283437408
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1300293909661523,
+          "pitch": 0.11417852961383801,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9314947258794799,
+          "pitch": 0.12188998399294526,
+          "title": "Роман Викторович",
+          "text": "Шанс спавна 0,0001%"
+        },
+        {
+          "yaw": -1.7751581638083973,
+          "pitch": 0.11261958845633657,
+          "title": "co-founder",
+          "text": "Илья наш программист"
+        },
+        {
+          "yaw": 1.5234293049391594,
+          "pitch": 1.3008320571984822,
+          "title": "Мои ноги",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.1992334834143534,
+        "pitch": 0.026878055237398613,
+        "fov": 1.4308103283437408
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.337059134935238,
+          "pitch": 0.08200576155472739,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 2.780479754132317,
+          "pitch": 0.09157262134106503,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.006310634311255,
+          "pitch": -0.09350954812421008,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -0.2371159995509018,
+          "pitch": 0.06951949833504045,
+          "rotation": 0,
+          "target": "3-pxl_20240201_155650015photosphere"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3604787569271206,
+          "pitch": 0.24925938473611353,
+          "title": "Тут мы играем в шахматы",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-pxl_20240201_155650015photosphere",
+      "name": "PXL_20240201_155650015.PHOTOSPHERE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4697009300442572,
+          "pitch": 0.034775894104452476,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07137260322733141,
+          "pitch": 0.052061745082303545,
+          "title": "Выход",
+          "text": ""
+        },
+        {
+          "yaw": 1.829716561901539,
+          "pitch": 0.07816735260530194,
+          "title": "Хайтек",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
